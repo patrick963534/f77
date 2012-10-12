2999a7a047d4ee1fd80c8f577935398c14846c8f
patrick <Patrick_DJ@163.com>
2012-10-12 08:43:50 +0800

    .

--- libks/platform/include/ks/sys/system.h
+++ libks/platform/include/ks/sys/system.h
@@ -2,9 +2,12 @@
 #define __KS_SYS_SYSTEM_H__
 
 #include <ks/defs.h>
+#include <ks/object.h>
+#include <ks/container.h>
 
-#define ks_extends_sys_system()    \
-    void    (init*)();          \
+#define ks_extends_sys_system()                     \
+    ks_extends_object();                            \
+    void    (init*)(ks_container_t* container);     \
     void    (create_window*)(const char* title, int w, int h)
 
 typedef struct ks_sys_system_t
