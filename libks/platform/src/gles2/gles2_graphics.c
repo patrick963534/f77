a619b8ecedcf0751e34ba67d17d466b7d18caa2b
patrick <patrick_dj@163.com>
2012-10-19 11:26:43 +0800

    .

--- libks/platform/src/gles2/gles2_graphics.c
+++ libks/platform/src/gles2/gles2_graphics.c
@@ -6,9 +6,24 @@
 #include <GLES2/gl2.h>
 #include <stdlib.h>
 
+#include "gles2_shader.h"
+
+typedef struct tex_program_t
+{
+    GLuint      program;
+
+    GLuint      texture_id;
+    GLint       positionLoc;
+    GLint       texCoordLoc;
+    GLint       samplerLoc;
+
+} tex_program_t;
+
 typedef struct graphics_t
 {
     ks_extends_graphics();
+
+    tex_program_t tex_render;
 } graphics_t;
 
 static graphics_t* g = 0;
@@ -80,8 +95,8 @@ static int test_init()
 
     glAttachShader(programObject, vertexShader);
     glAttachShader(programObject, fragmentShader);
-    glBindAttribLocation(programObject, 0, "vPosition");
     glLinkProgram(programObject);
+    glBindAttribLocation(programObject, 0, "vPosition");
 
     // Check the link status
     glGetProgramiv(programObject, GL_LINK_STATUS, &linked);
@@ -105,6 +120,9 @@ static int test_init()
         return GL_FALSE;
     }
 
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
     return GL_TRUE;
 }
@@ -126,11 +144,71 @@ static void test_draw()
     glDrawArrays(GL_TRIANGLES, 0, 3);
 }
 
+static GLuint CreateSimpleTexture2D()
+{
+    GLuint textureId;
+
+    GLubyte pixels[4 * 3] = { 255,   0,   0,  // Red
+                              0, 255,   0,    // Green
+                              0,   0, 255,    // Blue
+                              255, 255,   0   // Yellow
+    };
+
+    // Use tightly packed data
+    glPixelStorei ( GL_UNPACK_ALIGNMENT, 1 );
+    glGenTextures ( 1, &textureId );
+    glBindTexture ( GL_TEXTURE_2D, textureId );
+    glTexImage2D ( GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels );
+    glTexParameteri ( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
+    glTexParameteri ( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
+
+    return textureId;
+}
+
 static void draw(int x, int y)
 {
+    GLfloat vVertices[] = {-0.5f,  0.5f, 0.0f, // Position 0
+                            0.0f,  0.0f,       // TexCoord 0 
+                           -0.5f, -0.5f, 0.0f, // Position 1
+                            0.0f,  1.0f,       // TexCoord 1
+                            0.5f, -0.5f, 0.0f, // Position 2
+                            1.0f,  1.0f,       // TexCoord 2
+                            0.5f,  0.5f, 0.0f, // Position 3
+                            1.0f,  0.0f        // TexCoord 3
+    };
+    GLushort indices[] = { 0, 1, 2, 0, 2, 3 };
+
+    if (g->tex_render.texture_id == 0)
+        g->tex_render.texture_id = CreateSimpleTexture2D();
+
+    glViewport(0, 0, ks_director_instance()->width, ks_director_instance()->height);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    if (g->tex_render.program == 0)
+    {
+        g->tex_render.program = ks_gles2_shader_program_for(ks_gles2_program_type_texture);
+        g->tex_render.positionLoc = glGetAttribLocation(g->tex_render.program, "a_position");
+        g->tex_render.texCoordLoc = glGetAttribLocation(g->tex_render.program, "a_texCoord");
+        g->tex_render.samplerLoc = glGetUniformLocation(g->tex_render.program, "s_texture");
+    }
+
+    glUseProgram(g->tex_render.program);
+
+    glVertexAttribPointer(g->tex_render.positionLoc, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), vVertices);
+    glVertexAttribPointer(g->tex_render.texCoordLoc, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), &vVertices[3]);
+
+    glEnableVertexAttribArray(g->tex_render.positionLoc);
+    glEnableVertexAttribArray(g->tex_render.texCoordLoc);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, g->tex_render.texture_id);
+    glUniform1i(g->tex_render.samplerLoc, 0);
+
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, indices);
+
     ks_unused(x);
     ks_unused(y);
-    test_draw();
+    //test_draw();
 }
 
 static void destruct(graphics_t* me)
---
+++ libks/platform/src/gles2/gles2_shader.c
@@ -0,0 +1,125 @@
+#include "gles2_shader.h"
+#include <ks/log.h>
+#include <stdlib.h>
+
+typedef struct program_t
+{
+    const char* vs;
+    const char* fs;
+} program_t;
+
+static program_t tex_program = {
+    "attribute vec4 a_position;   \n"
+    "attribute vec2 a_texCoord;   \n"
+    "varying vec2 v_texCoord;     \n"
+    "void main()                  \n"
+    "{                            \n"
+    "   gl_Position = a_position; \n"
+    "   v_texCoord = a_texCoord;  \n"
+    "}                            \n",
+
+    "precision mediump float;                            \n"
+    "varying vec2 v_texCoord;                            \n"
+    "uniform sampler2D s_texture;                        \n"
+    "void main()                                         \n"
+    "{                                                   \n"
+    "  gl_FragColor = texture2D(s_texture, v_texCoord);  \n"
+    "}                                                   \n",
+};
+
+static GLuint load_shader(GLenum type, const char *shaderSrc)
+{
+    GLuint shader = 0;
+    GLint compiled;
+
+    if (0 == (shader = glCreateShader(type)))
+        return 0;
+
+    glShaderSource(shader, 1, &shaderSrc, NULL);
+    glCompileShader(shader);
+
+    // Check the compile status
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
+    if (!compiled)
+    {
+        GLint infoLen = 0;
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
+        if (infoLen > 1)
+        {
+            char* infoLog = malloc(sizeof(char) * infoLen);
+            glGetShaderInfoLog ( shader, infoLen, NULL, infoLog);
+            ks_log("Error compiling shader:\n%s\n", infoLog);
+            free(infoLog);
+        }
+        glDeleteShader(shader);
+        return 0;
+    }
+
+    return shader;
+}
+
+GLuint ks_gles2_shader_program_from(const char* vs_str, const char* fs_str)
+{
+    GLuint vs = 0;
+    GLuint fs = 0;
+    GLuint p  = 0;
+    GLint linked;
+
+    if (0 == (vs = load_shader(GL_VERTEX_SHADER, vs_str)))
+        goto fail;
+
+    if (0 == (fs = load_shader(GL_FRAGMENT_SHADER, fs_str)))
+        goto fail;
+
+    if (0 == (p = glCreateProgram()))
+        goto fail;
+
+    glAttachShader(p, vs);
+    glAttachShader(p, fs);
+    glLinkProgram(p);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+    vs = 0;
+    fs = 0;
+
+    // Check the link status
+    glGetProgramiv(p, GL_LINK_STATUS, &linked);
+    if (!linked) 
+    {
+        GLint infoLen = 0;
+        glGetProgramiv(p, GL_INFO_LOG_LENGTH, &infoLen);
+        if (infoLen > 1)
+        {
+            char* infoLog = malloc(infoLen);
+            glGetProgramInfoLog(p, infoLen, NULL, infoLog);
+            ks_log("Error linking program:\n%s\n", infoLog);
+            free(infoLog);
+        }
+        glDeleteProgram(p);
+        return 0;
+    }
+
+    return p;
+
+fail:
+    ks_log("%s", "fail to load program or shader");
+
+    if (vs != 0)
+        glDeleteShader(vs);
+    if (fs != 0)
+        glDeleteShader(fs);
+
+    return 0;
+}
+
+GLuint ks_gles2_shader_program_for(ks_gles2_program_type_e type)
+{
+    switch (type)
+    {
+    case ks_gles2_program_type_texture:
+        return ks_gles2_shader_program_from(tex_program.vs, tex_program.fs);
+    default:
+        return 0;
+    }
+}
