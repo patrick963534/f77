c0ef7384dc1bf77f3abe071573e411cb63c8963a
patrick <patrick_dj@163.com>
2012-10-19 11:30:17 +0800

    .

--- libks/platform/src/gles2/gles2_graphics.c
+++ libks/platform/src/gles2/gles2_graphics.c
@@ -29,121 +29,6 @@ typedef struct graphics_t
 static graphics_t* g = 0;
 static GLuint programObject = 0;
 
-static GLuint test_load_shader(GLenum type, const char *shaderSrc)
-{
-    GLuint shader;
-    GLint  compiled;
-
-    shader = glCreateShader(type);
-
-    if ( shader == 0 )
-        return 0;
-
-    glShaderSource(shader, 1, &shaderSrc, NULL);
-    glCompileShader(shader);
-
-    // Check the compile status
-    glGetShaderiv(shader, GL_COMPILE_STATUS, &compiled);
-    if (!compiled) 
-    {
-        GLint infoLen = 0;
-        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &infoLen);
-
-        if (infoLen > 1)
-        {
-            char* infoLog = malloc(sizeof(char) * infoLen );
-
-            glGetShaderInfoLog(shader, infoLen, NULL, infoLog);
-            ks_log("Error compiling shader:\n%s\n", infoLog);
-
-            free(infoLog);
-        }
-
-        glDeleteShader(shader);
-        return 0;
-    }
-
-    return shader;
-}
-
-static int test_init()
-{
-    char vShaderStr[] =  
-        "attribute vec4 vPosition;    \n"
-        "void main()                  \n"
-        "{                            \n"
-        "   gl_Position = vPosition;  \n"
-        "}                            \n";
-
-    char fShaderStr[] =  
-        "precision mediump float;                     \n"
-        "void main()                                  \n"
-        "{                                            \n"
-        "  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);   \n"
-        "}                                            \n";
-
-    GLuint vertexShader;
-    GLuint fragmentShader;
-    GLint  linked;
-
-    vertexShader = test_load_shader(GL_VERTEX_SHADER, vShaderStr);
-    fragmentShader = test_load_shader(GL_FRAGMENT_SHADER, fShaderStr);
-
-    programObject = glCreateProgram();
-    if (programObject == 0)
-        return 0;
-
-    glAttachShader(programObject, vertexShader);
-    glAttachShader(programObject, fragmentShader);
-    glLinkProgram(programObject);
-    glBindAttribLocation(programObject, 0, "vPosition");
-
-    // Check the link status
-    glGetProgramiv(programObject, GL_LINK_STATUS, &linked);
-    if (!linked) 
-    {
-        GLint infoLen = 0;
-
-        glGetProgramiv(programObject, GL_INFO_LOG_LENGTH, &infoLen);
-
-        if (infoLen > 1)
-        {
-            char* infoLog = malloc(sizeof(char) * infoLen);
-
-            glGetProgramInfoLog(programObject, infoLen, NULL, infoLog);
-            ks_log("Error linking program:\n%s\n", infoLog);
-
-            free(infoLog);
-        }
-
-        glDeleteProgram(programObject);
-        return GL_FALSE;
-    }
-
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-    return GL_TRUE;
-}
-
-static void test_draw()
-{
-    GLfloat vVertices[] = { 0.0f,  0.5f, 0.0f, 
-        -0.5f, -0.5f, 0.0f,
-        0.5f, -0.5f, 0.0f };
-
-    glViewport(0, 0, ks_director_instance()->width, ks_director_instance()->height);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    glUseProgram(programObject);
-
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, vVertices);
-    glEnableVertexAttribArray(0);
-
-    glDrawArrays(GL_TRIANGLES, 0, 3);
-}
-
 static GLuint CreateSimpleTexture2D()
 {
     GLuint textureId;
@@ -155,12 +40,12 @@ static GLuint CreateSimpleTexture2D()
     };
 
     // Use tightly packed data
-    glPixelStorei ( GL_UNPACK_ALIGNMENT, 1 );
-    glGenTextures ( 1, &textureId );
-    glBindTexture ( GL_TEXTURE_2D, textureId );
-    glTexImage2D ( GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels );
-    glTexParameteri ( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
-    glTexParameteri ( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    glGenTextures(1, &textureId);
+    glBindTexture(GL_TEXTURE_2D, textureId);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
     return textureId;
 }
@@ -240,7 +125,7 @@ KS_API void ks_graphics_init(ks_container_t* container)
     if (container)
         ks_container_add(container, (ks_object_t*)g);
 
-    test_init();
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 }
 
 KS_API ks_graphics_t* ks_graphics_instance()
