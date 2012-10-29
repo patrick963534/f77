cfa6baccb3be8e8b48a8ae1a5ef9a7e15262a8ab
patrick <patrick_dj@163.com>
2012-10-29 14:53:43 +0800

    .

--- libks/src/main.c
+++ libks/src/main.c
@@ -6,9 +6,14 @@
 #include <string.h>
 #include <ks/director.h>
 #include <ks/scene.h>
+#include <ks/zip.h>
 
 int main()
 {
+    const char* data = "adsfjoaewijfaowegjagael;fjeiowfoasdfaeoifjawoegj;eig";
+    int ret = 0;
+    ks_zip_compress(data, strlen(data) + 1, &ret);
+
     ks_director_init("Hello World", 320, 240);
     ks_director_run(ks_scene_new(sizeof(ks_scene_t)));
     return 0;
--- libks/src/zip/hufman/hufman.c
+++ libks/src/zip/hufman/hufman.c
@@ -1,12 +1,71 @@
 #include "hufman.h"
+#include <ks/log.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define LEAF_MAX    256
+#define NODE_MAX    512
 
 typedef struct node_t
 {
-    int a;
+    int     point;
+    char    val;
+
+    struct node_t* left;
+    struct node_t* right;
 } node_t;
+
+typedef struct hufman_t
+{
+    node_t*  nodes[NODE_MAX];
+    int      total;
+    int      current;
+    int      count;
+
+    char     leafs[LEAF_MAX];
+    int      nleaf;
+} hufman_t;
+
+static int node_comparer(const void* v1, const void* v2)
+{
+    node_t* n1 = (node_t*)v1;
+    node_t* n2 = (node_t*)v2;
+    
+    return n1->point < n2->point;
+}
 
 char* ks_zip_hufman_compress(const char* data, int sz, int* ret_sz)
 {
+    hufman_t* hm;
+    int       i, k;
+    int       dict[256];
+
+    memset(dict, 0, sizeof(dict));
+    for (i = 0; i < sz; i++)
+        dict[(unsigned int)data[i]] += 1;
+
+    hm = calloc(1, sizeof(*hm));
+    for (i = 0; i < LEAF_MAX; i++)
+    {
+        if (dict[i] != 0)
+        {
+            k = hm->nleaf;
+            hm->nodes[k] = calloc(1, sizeof(hm->nodes[0]));
+            hm->nodes[k]->point = dict[i];
+            hm->nodes[k]->val = (char)i;
+
+            hm->leafs[k] = hm->nodes[k]->val;
+            hm->nleaf++;
+
+            ks_log("0x%02x : %d", hm->nodes[k]->val, hm->nodes[k]->point);
+        }
+    }
+
+    qsort(hm->nodes, hm->nleaf, sizeof(hm->nodes[0]), node_comparer);
+
+    for (i = 0; i < hm->nleaf; i++)
+        ks_log("0x%02x : %d", hm->nodes[i]->val, hm->nodes[i]->point);
+    
     ks_unused(data);
     ks_unused(sz);
     ks_unused(ret_sz);
