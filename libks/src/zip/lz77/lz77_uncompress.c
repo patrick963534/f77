73fb2bae1ade335deeb17c4c351758af73e24545
patrick <patrick_dj@163.com>
2012-11-09 17:04:03 +0800

    .

--- libks/src/zip/lz77/lz77_uncompress.c
+++ libks/src/zip/lz77/lz77_uncompress.c
@@ -1,5 +1,57 @@
 #include "lz77.c.h"
 
+static void generate(lz77_t* lz)
+{
+    uchar* bytes_uncompress;
+
+    uchar* bytes;
+    int    offset_bits;
+    int    length_bits;
+    int    bit;
+    int    nbit;
+    int    nbyte;
+    int    i;
+    uchar  b;
+
+    bytes = lz77_header_load(lz);
+
+    bytes_uncompress = lz->bytes_uncompressed;
+    offset_bits = lz->offset_bits;
+    length_bits = lz->length_bits;
+
+    nbit  = 0;
+    nbyte = 0;
+    b     = bytes[nbyte];  
+
+    while (nbyte < lz->nbyte_compressed)
+    {
+        int is_pair = ((b >> (7 - nbit)) & 0x1);
+        int to;
+
+        nbit++;
+        if (nbit == 8)
+        {
+            nbit = 0;
+            nbyte++;
+            b = bytes[nbyte];
+        }
+
+        to = 0;
+        for (i = 0; i < 8; i++)
+        {
+            if ((b >> (7 - nbit)) & 0x1)
+                to += 1 << (7 - i);
+
+
+        }
+
+        if (is_pair)
+        {
+            
+        }
+        
+    }
+}
 
 char* zip_lz77_uncompress(const char* data, int sz, int* ret_sz)
 {
@@ -11,10 +63,7 @@ char* zip_lz77_uncompress(const char* data, int sz, int* ret_sz)
     lz->bytes_compressed = (uchar*)data;
     lz->nbyte_compressed = sz;
 
-    lz77_header_load(lz);
+    generate(lz);
 
-    ks_unused(data);
-    ks_unused(sz);
-    ks_unused(ret_sz);
-    return 0;
+    return (char*)lz->bytes_uncompressed;
 }
