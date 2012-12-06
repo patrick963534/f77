3673953afb6e7d0e8e95021a8b083af278166b15
patrick <patrick_dj@163.com>
2012-12-06 15:16:22 +0800

    .

--- libks/src/zip/deflate/deflate_compress.c
+++ libks/src/zip/deflate/deflate_compress.c
@@ -16,90 +16,6 @@ static void out(uint data, uint len)
 	g_bitoffset += len;
 }
 
-static void encode_offset(uint offset)
-{
-	static uint o;
-	static uint is_init = 0;
-	static uint offset_cache[32769];
-
-	if (!is_init)
-	{
-		static const unsigned short dists[30] = {
-			1, 2, 3, 4, 5, 7, 9, 13, 17, 25, 33, 49, 65, 97, 129, 193,
-			257, 385, 513, 769, 1025, 1537, 2049, 3073, 4097, 6145,
-			8193, 12289, 16385, 24577};
-		static const unsigned short dext[30] = {
-			0, 0, 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6,
-			7, 7, 8, 8, 9, 9, 10, 10, 11, 11,
-			12, 12, 13, 13};
-	        
-   		uint i, extra, i2, offset;
-		for(offset = 1; offset <= 32768; offset++)
-		{
-	   		for(i = 0; i < 30 && offset >= dists[i]; i++);
-			i--;
-
-			i2 = revbits(i, 5);
-			extra = offset - dists[i];
-			i2 |= (extra << 5);	
-		
-			offset_cache[offset] = i2 | ((dext[i] + 5) << 24);
-		}
-		is_init = 1;
-	}
-	o = offset_cache[offset];
-	out(o & 0xffffff, o >> 24); 
- 
-}
-
-static void encode_literal(uint lit)
-{
-    out(static_huffman_cache->code_bits[lit], static_huffman_cache->code_nbit[lit]);
-}
-
-static void get_length_code(uint matchlen)
-{
-	uint extra, i, code;
-    static const unsigned short lens[29] = {
-        3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 15, 17, 19, 23, 27, 31,
-        35, 43, 51, 59, 67, 83, 99, 115, 131, 163, 195, 227, 258};
-    static const unsigned short lext[29] = {
-        0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2,
-        3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 0};
-        
-	static const unsigned short matchlens[11] = {0, 0, 0, 64, 32, 96, 16, 80, 48, 112, 8};
-
-	if(matchlen <= 10)
-	{
-		out(matchlens[matchlen], 7);
-	}
-	else
-	{
-		for(i = 0; i < 29 && matchlen >= lens[i]; i++);
-		i--;
-		code = i + 254 + 3; // 257...285 i1 = CODE
-		
-		if(code < 280)
-		{
-            extra = matchlen - lens[i];
-
-			code = revbits(code, 7);
-			code |= (extra << 7);
-			out(code, 7 + lext[i]);
-		}
-		else
-		{
-            extra = matchlen - lens[i];	
-
-			code = code + 192 - 280;
-			code = revbits(code, 8);
-			code |= (extra << 8);
-			out(code, 8 + lext[i]);
-		}
-	}
-}
-
-
 static uint deflate(uchar *source, uchar *destination, uint bitoffset, uint size, uint last)
 {
 	static uint i;
@@ -117,8 +33,8 @@ static uint deflate(uchar *source, uchar *destination, uint bitoffset, uint size
 
 	if(size > 0)
 	{
-		encode_literal(*src);
-		src++;
+        out(static_huffman_cache->code_bits[*src], static_huffman_cache->code_nbit[*src]);
+        src++;
 	}
 
 	while(src <= last_source_byte)
@@ -137,13 +53,14 @@ static uint deflate(uchar *source, uchar *destination, uint bitoffset, uint size
 			while(src + n <= last_source_byte && *(o + n) == *(src + n) && n < 257)
 				n++;
 				
-			get_length_code(n);
-			encode_offset((uint)(src - o));
+            out(length_cache->code_bits[n], length_cache->code_nbit[n]);
+            out(offset_cache->code_bits[(uint)(src - o)], offset_cache->code_nbit[(uint)(src - o)]);
+
 			src += n;
 		}
 		else
 		{
-			encode_literal(*src);
+            out(static_huffman_cache->code_bits[*src], static_huffman_cache->code_nbit[*src]);
 			src++;
 		}
 	}
