9e155974a910122dc4fc985c66ddc4068ee38a6b
patrick <patrick_dj@163.com>
2013-02-21 19:17:22 +0800

    [libks] - fix wrong function link.

--- libks/src/zip/deflate_draft/deflate_draft.c
+++ libks/src/zip/deflate_draft/deflate_draft.c
@@ -209,7 +209,7 @@ static unsigned char fn[1000];
 static unsigned int k, file_source_len, file_compressed_len, remain;
 static unsigned int num_files, first_data_add;
 
-unsigned int crc32(unsigned char *data, unsigned int size, unsigned int crc)
+static unsigned int crc32(unsigned char *data, unsigned int size, unsigned int crc)
 {
 	static const unsigned int table[256] = {
 	0x00000000, 0x77073096, 0xee0e612c, 0x990951ba,0x076dc419, 0x706af48f, 0xe963a535, 0x9e6495a3,0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988,0x09b64c2b, 0x7eb17cbd, 0xe7b82d07, 0x90bf1d91,
