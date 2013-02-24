fd312cc0ad760a509d075822f0353274f3b0bed6
Patrick <Patrick_DJ@163.com>
2013-02-24 12:18:45 +0800

    .

--- libks/src/main.c
+++ libks/src/main.c
@@ -15,12 +15,20 @@ static ks_scene_t* create_scene()
     char buf[256];
 
     ks_scene_t* me;
-    ks_actor_t* actor;
+    ks_actor_t* actor0;
+    ks_actor_t* actor1;
+    ks_actor_t* actor2;
 
     ks_helper_path_join_relative_app(buf, sizeof(buf), "img.png");
 
     me = ks_scene_new(sizeof(*me));
-    actor = ks_actor_new(buf, sizeof(*actor), (ks_node_t*)me);
+    actor0 = ks_actor_new(buf, sizeof(*actor0), (ks_node_t*)me);
+    actor1 = ks_actor_new(buf, sizeof(*actor1), (ks_node_t*)me);
+    actor2 = ks_actor_new(buf, sizeof(*actor2), (ks_node_t*)me);
+
+    actor0->x = 0;
+    actor1->x = 400;
+    actor2->x = 800;
 
     return me;
 }
