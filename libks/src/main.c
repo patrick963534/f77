13f9cbd28eba9811a67881656bf062a09cade182
Patrick <Patrick_DJ@163.com>
2013-02-24 21:38:23 +0800

    .

--- libks/src/main.c
+++ libks/src/main.c
@@ -8,7 +8,39 @@
 #include <ks/scene.h>
 #include <ks/actor.h>
 #include <ks/helper.h>
+#include <ks/event.h>
+#include <ks/constants.h>
 #include "utest/utest.h"
+
+static void step(ks_node_t* me, int delta)
+{
+    static float offset = 0;
+
+    offset += delta * 50 / 1000.0f;
+
+    if (offset > 1)
+    {
+        me->y += (int)offset;
+        offset -= (int)offset;
+    }
+}
+
+static int msgs(ks_node_t* me, ks_event_t* e)
+{
+    if (e->type == ks.types.KEY_DOWN)
+    {
+        if (e->key.code == ks.keys.Up)
+            ks_log("arrow up");
+        else if (e->key.code == ks.keys.Down)
+            ks_log("arrow down");
+        else if (e->key.code == ks.keys.Left)
+            ks_log("arrow left");
+        else if (e->key.code == ks.keys.Right)
+            ks_log("arrow right");
+    }
+
+    return 0;
+}
 
 static ks_scene_t* create_scene()
 {
@@ -29,6 +61,9 @@ static ks_scene_t* create_scene()
     actor0->x = 0;
     actor1->x = 400;
     actor2->x = 800;
+
+    actor0->step = step;
+    actor0->msgs = msgs;
 
     return me;
 }
