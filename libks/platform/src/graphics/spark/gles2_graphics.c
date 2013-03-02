9e724584398b299895c8fd3644fe976fb44e41fa
patrick <patrick_dj@163.com>
2013-03-02 17:50:22 +0800

    [spark] - add coordinate bounding.

--- libks/platform/src/graphics/spark/gles2_graphics.c
+++ libks/platform/src/graphics/spark/gles2_graphics.c
@@ -30,6 +30,48 @@ typedef struct graphics_t
 
 static graphics_t* g = 0;
 
+static int bounding(int* x, int* y, int* cx, int* cy, int* cw, int* ch)
+{
+    int win_width = ks_director_instance()->width;
+    int win_height = ks_director_instance()->height;
+
+    int minx = *x;
+    int miny = *y;
+    int maxx = minx + *cw;
+    int maxy = miny + *ch;
+
+    if (minx >= win_width || maxx <= 0 || miny >= win_height || maxy <= 0)
+        return 0;
+
+    if (minx < 0)
+        minx = 0;
+
+    if (maxx > win_width)
+        maxx = win_width;
+
+    if (miny < 0)
+        miny = 0;
+
+    if (maxy > win_height)
+        maxy = win_height;
+
+    if (minx != *x)
+        *cx += (minx - *x);
+
+    if (miny != *y)
+        *cy += (miny - *y);
+
+    *x = minx;
+    *y = miny;
+    *cw = maxx - minx;
+    *ch = maxy - miny;
+
+    if (*cw == 0 || *ch == 0)
+        return 0;
+
+    return 1;
+}
+
 static void draw(ks_image_t* img, int offx, int offy, int clip_x, int clip_y, int clip_w, int clip_h)
 {
     int win_width = ks_director_instance()->width;
@@ -43,18 +85,28 @@ static void draw(ks_image_t* img, int offx, int offy, int clip_x, int clip_y, in
     int y = g->pos.y + offy;
     int i;
 
-    x = 0;
-    y = 0;
+    if (!bounding(&x, &y, &clip_x, &clip_y, &clip_w, &clip_h))
+        return;
 
     for (i = 0; i < clip_h; ++i) 
     {
         memcpy(&dst[(i + y) * dst_strip + x * 4], &src[(i + clip_y) * src_strip + clip_x * 4], clip_w * 4);
+//         int j;
+//         for (j = 0; j < clip_w; ++j)
+//         {
+//             dst[(i + y) * dst_strip + x * 4 + j * 4 + 0] = src[(i + clip_y) * src_strip + clip_x * 4 + j * 4 + 0];
+//             dst[(i + y) * dst_strip + x * 4 + j * 4 + 1] = src[(i + clip_y) * src_strip + clip_x * 4 + j * 4 + 1];
+//             dst[(i + y) * dst_strip + x * 4 + j * 4 + 2] = src[(i + clip_y) * src_strip + clip_x * 4 + j * 4 + 2];
+//             dst[(i + y) * dst_strip + x * 4 + j * 4 + 3] = src[(i + clip_y) * src_strip + clip_x * 4 + j * 4 + 3];
+//         }
     }
 }
 
 static void clear_screen()
 {
-    
+    int w = ks_director_instance()->width;
+    int h = ks_director_instance()->height;
+    memset(g->buffer, 0, w * h * 4);
 }
 
 static void graphics_translate(int x, int y, int z)
