15eaef6667e24ee290c6aa72b18c5ab21c9169bf
patrick <patrick_dj@163.com>
2013-03-16 17:34:00 +0800

    .

--- libks/src/main.c
+++ libks/src/main.c
@@ -156,13 +156,122 @@ static void test()
     ks_helper_image565_save_ppm("ab.ppm", buf, width, height);
 }
 
+static GLuint TexObj[2];
+static GLfloat Angle = 0.0f;
+
+static int cnt=0,v=0;
+
+void bind_texture(int texobj,int image)
+{
+  static int width = 8, height = 8;
+  static int color[2][3]={
+    {255,0,0},
+    {0,255,0},
+  };
+  GLubyte tex[64][3];
+  static GLubyte texchar[2][8*8] = {
+  {
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 1, 0, 0, 0,
+    0, 0, 0, 1, 1, 0, 0, 0,
+    0, 0, 0, 0, 1, 0, 0, 0,
+    0, 0, 0, 0, 1, 0, 0, 0,
+    0, 0, 0, 0, 1, 0, 0, 0,
+    0, 0, 0, 1, 1, 1, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0},
+  {
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 2, 2, 0, 0, 0,
+    0, 0, 2, 0, 0, 2, 0, 0,
+    0, 0, 0, 0, 0, 2, 0, 0,
+    0, 0, 0, 0, 2, 0, 0, 0,
+    0, 0, 0, 2, 0, 0, 0, 0,
+    0, 0, 2, 2, 2, 2, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0}};
+
+  int i,j;
+  
+  glBindTexture(GL_TEXTURE_2D, texobj);
+
+  /* red on white */
+  for (i = 0; i < height; i++) {
+    for (j = 0; j < width; j++) {
+      int p = i * width + j;
+      if (texchar[image][(height - i - 1) * width + j]) {
+        tex[p][0] = color[image][0];
+        tex[p][1] = color[image][1];
+        tex[p][2] = color[image][2];
+      } else {
+        tex[p][0] = 255;
+        tex[p][1] = 255;
+        tex[p][2] = 255;
+      }
+    }
+  }
+  glTexImage2D(GL_TEXTURE_2D, 0, 3, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, tex);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  /* end of texture object */
+}
+
+static void test2()
+{
+    ZBuffer *zb;
+    char* buf;
+    int width = 320;
+    int height = 240;
+
+    buf = calloc(1, width * height * 2);
+    zb = ZB_open(width, height, ZB_MODE_RGBA, 0, NULL, NULL, NULL);
+
+    glInit(zb);
+    glViewport(0, 0, width, height);
+    ZB_resize(zb, buf, width, height);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glFrustum(-1.0, 1.0, -1.0, 1.0, 5.0, 15.0);
+    glMatrixMode(GL_MODELVIEW);
+
+    glEnable(GL_DEPTH_TEST);
+
+    /* Setup texturing */
+    glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_FASTEST);
+
+    /* generate texture object IDs */
+    glGenTextures(2, TexObj);
+    bind_texture(TexObj[0],0);
+    bind_texture(TexObj[1],1);
+
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+    glEnable(GL_TEXTURE_2D);
+
+    glPushMatrix();
+    glLoadIdentity();
+    glTranslatef(0, 0, -10);
+    glBegin(GL_TRIANGLES);
+        glTexCoord2f(0.0, 0.0); glVertex3f( 0.0f,  0.5f, 0.f);
+        glTexCoord2f(1.0, 0.0); glVertex3f( 0.5f, -0.5f, 0.f);
+        glTexCoord2f(1.0, 1.0); glVertex3f(-0.5f, -0.5f, 0.f);
+    glEnd();
+    glPopMatrix();
+    glDisable(GL_TEXTURE_2D);
+
+    glFlush();
+
+    ks_helper_image565_save_ppm("ab.ppm", buf, width, height);
+}
+
 int main(int argc, char** argv)
 {
     ks_director_init("Hello World", 320, 240, argc, argv);
 
     ks_utest_start();
 
-    test();
-    ks_director_run(create_scene());
+    test2();
+    //ks_director_run(create_scene());
     return 0;
 }
