5c9bef04c96b9bd0b340e774ea477f4ba18848f1
patrick <patrick_dj@163.com>
2013-04-05 10:28:10 +0800

    .

--- libks/src/main.c
+++ libks/src/main.c
@@ -18,6 +18,13 @@
 
 #include <vld.h>
 
+#define Win_W   1024
+#define Win_H   768
+
+static ks_actor_t* actor0;
+static ks_actor_t* actor1;
+static ks_actor_t* actor2;
+
 static void step(ks_node_t* me, int delta)
 {
     static float offset = 0;
@@ -33,14 +40,14 @@ static void step(ks_node_t* me, int delta)
 
 static void step_rotate(ks_node_t* me, int delta)
 {
-    static float angle = 0;
+    static float time = 0;
 
-    angle += delta * 45 / 1000.0f;
+    time += delta;
 
-    if (angle > 1)
+    if (time > 3000)
     {
-        me->angle += (int)angle;
-        angle -= (int)angle;
+        me->angle += 20;
+        time -= 3000;
     }
 }
 
@@ -68,13 +75,13 @@ static int msgs(ks_node_t* me, ks_event_t* e)
     if (e->type == ks.types.KEY_DOWN)
     {
         if (e->key.code == ks.keys.Up)
-            me->y += 20;
+            actor0->angle += 20;
         else if (e->key.code == ks.keys.Down)
-            me->y -= 20;
+            actor0->angle -= 20;
         else if (e->key.code == ks.keys.Left)
-            me->x -= 20;
+        {actor0->sx -= 0.1f; actor0->sy -= 0.1f; }
         else if (e->key.code == ks.keys.Right)
-            me->x += 20;
+        {actor0->sx += 0.1f; actor0->sy += 0.1f; }
     }
 
     return 0;
@@ -84,49 +91,80 @@ static ks_scene_t* create_scene()
 {
     ks_scene_t* me;
     ks_node_t*  node;
-    ks_actor_t* actor0;
-    ks_actor_t* actor1;
-    ks_actor_t* actor2;
 
     me       = ks_scene_new(sizeof(*me));
     me->msgs = msgs;
 
-    node = ks_node_new(sizeof(*node), (ks_node_t*)me);
-    node->x = 100;
-    node->z = -1;
+//     node = ks_node_new(sizeof(*node), (ks_node_t*)me);
+//     node->x = 100;
+//     node->z = -1;
+// 
+//     ks_text_new("arial.ttf", 18, 60, "",        node)->step = (ks_step_f)step_fps;
+//     ks_text_new("arial.ttf", 18, 60, "0",       node)->y = 200;
+//     ks_text_new("arial.ttf", 18, 60, "00",      node)->y = 60;
+//     ks_text_new("arial.ttf", 18, 60, "000",     node)->y = 90;
+//     ks_text_new("arial.ttf", 18, 60, "0000",    node)->y = 120;
+//     ks_text_new("arial.ttf", 18, 60, "00000",   node)->y = 150;
 
-    ks_text_new("arial.ttf", 18, 60, "",        node)->step = (ks_step_f)step_fps;
-    ks_text_new("arial.ttf", 18, 60, "0",       node)->y = 200;
-    ks_text_new("arial.ttf", 18, 60, "00",      node)->y = 60;
-    ks_text_new("arial.ttf", 18, 60, "000",     node)->y = 90;
-    ks_text_new("arial.ttf", 18, 60, "0000",    node)->y = 120;
-    ks_text_new("arial.ttf", 18, 60, "00000",   node)->y = 150;
+    actor0 = ks_actor_new("grid4.png", (ks_node_t*)me);
+    actor0->x = 300;
+    actor0->y = 200;
 
-    actor0 = ks_actor_new("img.png", (ks_node_t*)me);
-    actor0->x = 128;
-    actor0->y = 128;
-    actor0->angle = 45;
-    actor0->step = step_rotate;
+    actor1 = ks_actor_new("grid4.png", (ks_node_t*)me);
+    actor1->x = 300 + 256;
+    actor1->y = 200;
+//      actor0->sx = 0.333f;
+//      actor0->sy = 1.5f;
+//     actor0->angle = 0;
+   //actor0->step = step_rotate;
 
-    actor1 = ks_actor_new("img.png", (ks_node_t*)me);
-    actor1->x = 384 + 10;
-    actor1->y = 128;
- 
-    actor2 = ks_actor_new("img.png", (ks_node_t*)me);
-    actor2->x = 640 + 20;
-    actor2->y = 128;
-
-    //actor0->step = step;
+//       actor1 = ks_actor_new("a0002.png", (ks_node_t*)me);
+//       actor1->x = Win_W/2;
+//       actor1->y = Win_H/2;
+//   
+//      actor2 = ks_actor_new("kk.png", (ks_node_t*)me);
+//      actor2->x = 640 + 20;
+//      actor2->y = 128;
 
     ks_node_sort_by_z((ks_node_t*)me);
 
     return me;
 }
 
+static void generate_grid_img()
+{
+    int w = 256, h = 256;
+    int pitch = w * 4;
+    char* pixels = calloc(1, pitch * h);
+    int i, j;
+
+    for (i = 0; i < w; ++i)
+    {
+        if (i % 4 == 0)
+        {
+            for (j = 0; j < h; ++j)
+            {
+                *((int*)(&pixels[j * pitch + i * 4])) = 0xffffffff;
+            }
+        }
+    }
+
+     for (i = 0; i < h; ++i)
+     {
+         if (i % 4 == 0)
+         {
+             memset(&pixels[i * pitch], 255, pitch);
+         }
+     }
+
+    ks_helper_image_save_ppm("E:\\grid.ppm", pixels, w, h);
+}
+
 int main(int argc, char** argv)
 {
-    ks_director_init("Hello World", 800, 480, argc, argv);
+    ks_director_init("Hello World", Win_W, Win_H, argc, argv);
 
+    //generate_grid_img();
     ks_utest_start();
 
     ks_director_run(create_scene());
