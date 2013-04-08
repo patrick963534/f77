b2160fc09d5d909e48b417a93fccd9d90f59ca8c
patrick <patrick_dj@163.com>
2013-04-08 14:16:03 +0800

    [sl] - init.

---
+++ libks/include/sl/sl.h
@@ -0,0 +1,48 @@
+#ifndef __SKY_LIBRARY_H__
+#define __SKY_LIBRARY_H__
+
+#define SL_API      extern
+
+typedef enum sl_matrix_mode_e
+{
+    sl_matrix_mode_project,
+    sl_matrix_mode_model,
+} sl_matrix_mode_e;
+
+typedef enum sl_cull_face_mode_e
+{
+    sl_cull_face_mode_font,
+    sl_cull_face_mode_back,
+} sl_cull_face_mode_e;
+
+typedef enum sl_enable_mode_e
+{
+    sl_enable_mode_cull_face,
+} sl_enable_mode_e;
+
+SL_API void     sl_init();
+SL_API void     sl_flush();
+SL_API void     sl_clear_color(int r, int g, int b);
+SL_API void     sl_clear();
+
+SL_API void     sl_enable(sl_enable_mode_e mode);
+SL_API void     sl_disable(sl_enable_mode_e mode);
+
+SL_API void     sl_viewport(int x, int y, int w, int h);
+SL_API void     sl_frustum(float left, float right, float bottom, float top, float near, float far);
+
+SL_API void     sl_matrix_mode(sl_matrix_mode_e mode);
+SL_API void     sl_load_identity();
+SL_API void     sl_translate(float x, float y, float z);
+SL_API void     sl_rotate(float angle, float x, float y, float z);
+SL_API void     sl_scale(float x, float y, float z);
+SL_API void     sl_push_matrix();
+SL_API void     sl_pop_matrix();
+
+SL_API void     sl_cull_face(sl_cull_face_mode_e mode);
+SL_API void     sl_begin();
+SL_API void     sl_vertex(float x, float y, float z);
+SL_API void     sl_tex_coord(float u, float v);
+SL_API void     sl_end();
+
+#endif
---
+++ libks/src/sl/sl.c.h
@@ -0,0 +1,6 @@
+#ifndef __SKY_LIBRARY_C_H__
+#define __SKY_LIBRARY_C_H__
+
+
+
+#endif
