3b5930c96f246f34f9087764ec2fb0124231eaf8
patrick <patrick_dj@163.com>
2013-04-10 10:53:45 +0800

    .

--- libks/src/sl/slu.c
+++ libks/src/sl/slu.c
@@ -18,7 +18,6 @@ SL_API void slu_lookat(float eyex, float eyey, float eyez, float centerx, float 
 {
     float m[16];
     float x[3], y[3], z[3];
-    float mag;
 
     /* Make rotation matrix */
 
@@ -27,14 +26,7 @@ SL_API void slu_lookat(float eyex, float eyey, float eyez, float centerx, float 
     z[1] = eyey - centery;
     z[2] = eyez - centerz;
 
-    mag = (float)sqrtf(z[0] * z[0] + z[1] * z[1] + z[2] * z[2]);
-
-    if (mag) 
-    {
-        z[0] /= mag; 
-        z[1] /= mag; 
-        z[2] /= mag;
-    }
+    sl_normalize(z, z+1, z+2);
 
     /* Y vector */
     y[0] = upx; y[1] = upy; y[2] = upz;
@@ -52,21 +44,8 @@ SL_API void slu_lookat(float eyex, float eyey, float eyez, float centerx, float 
     /* cross product gives area of parallelogram, which is < 1.0 for
     * non-perpendicular unit-length vectors; so normalize x, y here
     */
-    mag = (float)sqrtf(x[0] * x[0] + x[1] * x[1] + x[2] * x[2]);
-    if (mag) 
-    {
-        x[0] /= mag; 
-        x[1] /= mag; 
-        x[2] /= mag;
-    }
-
-    mag = (float)sqrtf(y[0] * y[0] + y[1] * y[1] + y[2] * y[2]);
-    if (mag) 
-    {
-        y[0] /= mag;
-        y[1] /= mag;
-        y[2] /= mag;
-    }
+    sl_normalize(x, x + 1, x + 2);
+    sl_normalize(y, y + 1, y + 2);
 
 #define M(row,col)  m[col*4+row]
     M(0, 0) = x[0]; M(0, 1) = x[1]; M(0, 2) = x[2]; M(0, 3) = 0.0f;
