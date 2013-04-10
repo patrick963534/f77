1085ff7f59120a72feececf46bd8df170bb18b42
patrick <patrick_dj@163.com>
2013-04-10 10:45:25 +0800

    [sl] - add basic unit test for matrix.

--- libks/src/utest/tests/sl_test.c
+++ libks/src/utest/tests/sl_test.c
@@ -19,8 +19,78 @@
 #include <ks/constants.h>
 #include <sl/sl.h>
 
-static void matrix_test()
+static int matrix_equals(float* m1, float* m2)
 {
+    int i;
+    float epsilon = 0.000001f;
+
+    for (i = 0; i < 16; ++i)
+    {
+        float diff = (m1[i] - m2[i]);
+        if (diff > epsilon || diff < -epsilon)
+            return 0;
+    }
+
+    return 1;
+}
+
+static void test_result(float* m1, float* m2, const char* name)
+{
+    if (!matrix_equals(m1, m2))
+        ks_print_log("Error", "Test in function {%s} is fail.", name);
+}
+
+static void matrix_translate_test()
+{
+    float result[16] = {
+        1,    0,    0,   0,
+        0,    1,    0,   0,
+        0,    0,    1,   0,
+     0.5f, 0.3f, 0.2f,   1,
+    };
+
+    float buf[16];
+
+    sl_init();
+    sl_matrix_mode(sl_matrix_mode_model);
+    sl_load_identity();
+    sl_translate(0.5f, 0.3f, 0.2f);
+    sl_get_floatv(sl_param_name_matrix_model_view, buf);    
+    sl_close();
+
+    test_result(result, buf, __FUNCTION__);
+}
+
+static void matrix_scale_test()
+{
+    float result[16] = {
+        0.5f,    0,    0,   0,
+           0, 0.3f,    0,   0,
+           0,    0, 0.2f,   0,
+           0,    0,    0,   1,
+    };
+
+    float buf[16];
+
+    sl_init();
+    sl_matrix_mode(sl_matrix_mode_model);
+    sl_load_identity();
+    sl_scale(0.5f, 0.3f, 0.2f);
+    sl_get_floatv(sl_param_name_matrix_model_view, buf);    
+    sl_close();
+
+    test_result(result, buf, __FUNCTION__);
+}
+
+static void matrix_rotate_test()
+{
+    float result[16] = {
+         0,  0, -1,  0,
+         0,  1,  0,  0,
+         1,  0,  0,  0,
+        -5,  0,  0,  1,
+    };
+
     float buf[16];
 
     sl_init();
@@ -30,9 +100,13 @@ static void matrix_test()
     sl_translate(0, 0, -5);
     sl_get_floatv(sl_param_name_matrix_model_view, buf);    
     sl_close();
+
+    test_result(result, buf, __FUNCTION__);
 }
 
 void ks_utest_sl_test()
 {
-    matrix_test();
+    matrix_rotate_test();
+    matrix_scale_test();
+    matrix_translate_test();
 }
