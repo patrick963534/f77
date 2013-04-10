2c376ce6e2337d72c976799a5011ece3ac3c4a0b
patrick <patrick_dj@163.com>
2013-04-10 11:01:46 +0800

    [sl] - matrix unit test.

--- libks/src/utest/tests/sl_test.c
+++ libks/src/utest/tests/sl_test.c
@@ -55,6 +55,28 @@ static void test_result(float* m1, float* m2, const char* name)
         ks_print_log("Error", "Test in function {%s} is fail.", name);
 }
 
+static void matrix_load_identity_test()
+{
+    float result[16] = {
+        1,    0,    0,   0,
+        0,    1,    0,   0,
+        0,    0,    1,   0,
+        0,    0,    0,   1,
+    };
+
+    float buf[16];
+
+    sl_init();
+    sl_matrix_mode(sl_matrix_mode_model);
+    sl_load_identity();
+    sl_translate(0.5f, 0.3f, 0.2f);
+    sl_load_identity();
+    sl_get_floatv(sl_param_name_matrix_model_view, buf);    
+    sl_close();
+
+    test_result(result, buf, __FUNCTION__);
+}
+
 static void matrix_translate_test()
 {
     float result[16] = {
@@ -140,10 +162,48 @@ static void matrix_slu_perspective_test()
     test_result(result, buf, __FUNCTION__);
 }
 
+static void matrix_push_pop_test()
+{
+    float result[16] = {
+        0.5f,    0,    0,   0,
+           0, 0.3f,    0,   0,
+           0,    0, 0.2f,   0,
+           0,    0,    0,   1,
+    };
+
+    float identity[16] = {
+        1,    0,    0,   0,
+        0,    1,    0,   0,
+        0,    0,    1,   0,
+        0,    0,    0,   1,
+    };
+
+    float buf[16];
+
+    sl_init();
+    sl_matrix_mode(sl_matrix_mode_model);
+    sl_load_identity();
+    sl_scale(0.5f, 0.3f, 0.2f);
+    sl_push_matrix();
+    sl_load_identity();
+
+    sl_get_floatv(sl_param_name_matrix_model_view, buf);    
+    test_result(identity, buf, __FUNCTION__);
+
+    sl_pop_matrix();
+
+    sl_get_floatv(sl_param_name_matrix_model_view, buf);    
+    test_result(result, buf, __FUNCTION__);
+
+    sl_close();
+}
+
 void ks_utest_sl_test()
 {
     matrix_rotate_test();
     matrix_scale_test();
     matrix_translate_test();
     matrix_slu_perspective_test();
+    matrix_load_identity_test();
+    matrix_push_pop_test();
 }
